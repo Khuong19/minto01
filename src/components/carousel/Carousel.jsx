c1f78c28b84151fd75a1e136687c173aecd5d563
Khuong19 <133074738+Khuong19@users.noreply.github.com>
2023-10-05 11:13:40 +0700

    ok

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import laptop from '../../assets/socialMedia/Group 48098877.png'
+import pic from '../../assets/Carousel/Frame 48099763.png'
 import './Carousel.css'
 const Carousel = () => {
   return (
@@ -13,71 +14,9 @@ const Carousel = () => {
                 <p className='text-xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br> Vestibulum mollis nunc a molestie dictum. Mauris venenatis, felis <br></br>scelerisque aliquet lacinia, nulla nisi venenatis odio, id blandit</p>
             </div>
         </div>
-      <div id="default-carousel" className="relative w-full" data-carousel="slide">
-        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-          {/* <!-- Item 1 --> */}
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img
-              src="/docs/images/carousel/carousel-1.svg"
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-            />
-          </div>
-          {/* <!-- Item 2 --> */}
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img
-              src="/docs/images/carousel/carousel-2.svg"
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-            />
-          </div>
-          {/* <!-- Item 3 --> */}
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <img
-              src="/docs/images/carousel/carousel-3.svg"
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-            />
-          </div>
-        </div>
-        {/* <!-- Slider indicators --> */}
-        <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="true"
-            aria-label="Slide 1"
-            data-carousel-slide-to="0"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 2"
-            data-carousel-slide-to="1"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 3"
-            data-carousel-slide-to="2"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 4"
-            data-carousel-slide-to="3"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 5"
-            data-carousel-slide-to="4"
-          ></button>
-        </div>
+        
+      <div className="flex flex-col items-center w-full mt-10 pb-10">
+          <img src={pic} alt="" />
       </div>
     </div>
   );
